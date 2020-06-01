164448aeb1610e86bc832ac86583e7f880788239
Fahmi Rosdiansyah Mahdani <fashni@outlook.com>
2020-06-01 20:54:47 +0700

    add non-gui script

---
+++ script_manual.m
@@ -0,0 +1,52 @@
+pendulum = PendulumIntegrator();
+
+g = 9.8;
+m = [1 1];
+L = [1 1];
+th_0 = [pi/6 0];
+w_0 = [0 0];
+h = 0.005;
+iterasi = 2000;
+freq = 20;
+
+pendulum.add_properties('GravAcc', g, 'Steps', h, 'Iterations', iterasi, ...
+'Mass', m, 'Length', L, 'InitialTheta', th_0, 'InitialOmega', w_0);
+
+pendulum.runge_kutta();
+% pendulum.euler();
+% pendulum.symplectic_euler();
+t = 0:pendulum.steps:pendulum.steps*(pendulum.iterations-1);
+
+cartesian = pendulum.get_cartesian();
+x1 = cartesian(1, :);
+y1 = cartesian(2, :);
+x2 = cartesian(3, :);
+y2 = cartesian(4, :);
+
+%% Simulasi
+figure
+daspect([1 1 1]);
+set(gca, 'XLim', [-sum(pendulum.length)*1.2 sum(pendulum.length)*1.2], ... 
+         'YLim', [-sum(pendulum.length)*1.2 sum(pendulum.length)*1.2]);
+hold on
+for k=1:freq:pendulum.iterations
+  string1 = line([0 x1(k)], [0 y1(k)], 'LineWidth', 1.5, 'Color', '#A2142F');
+  string2 = line([x1(k) x2(k)], [y1(k) y2(k)], 'LineWidth', 1.5, 'Color', '#A2142F');
+  head1 = scatter(x1(k), y1(k), 70, 'filled', 'MarkerFaceColor', 	'#77AC30', 'MarkerEdgeColor', 'k');
+  head2 = scatter(x2(k), y2(k), 70, 'filled', 'MarkerFaceColor', '#7E2F8E', 'MarkerEdgeColor', 'k');
+  drawnow();
+  if k+freq <= pendulum.iterations
+    delete(head1);
+    delete(head2);
+    delete(string1);
+    delete(string2);
+  end
+end
+hold off
+
+%% Grafik Waktu
+figure
+plot(t, pendulum.th_data(1,:), t, pendulum.th_data(2,:))
+
+figure
+plot(t, pendulum.w_data(1,:), t, pendulum.w_data(2,:))
